bb3b0a8a85f2b7347e963631f5e535fd0d5b2cac
pop <pop@pop-os.localdomain>
2023-01-06 20:03:39 +0530

    added functions

---
+++ src/functions/StringExample.java
@@ -0,0 +1,11 @@
+package functions;
+
+public class StringExample {
+    static String greet(){
+        String greeting = "Hello";
+        return greeting;
+    }
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+}
---
+++ src/functions/Sum.java
@@ -0,0 +1,10 @@
+package functions;
+
+public class Sum {
+    static int sum(int num1, int num2){
+        return num1 + num2;
+    }
+    public static void main(String[] args) {
+        System.out.println("The sum is: "+sum(5,6));
+    }
+}
