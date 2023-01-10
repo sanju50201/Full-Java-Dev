599ea9cad2e187e318e53b973d91a568b21dbec3
sanju50201 <sanjithk50201@gmail.com>
2023-01-10 18:56:43 +0530

    added new practice problems

---
+++ src/practice/AverageArray.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class AverageArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int sum = 0;
+        int average = 0;
+        for(int i=0;i< arr.length;i++){
+            sum += arr[i];
+        }
+        average = sum/arr.length;
+        System.out.println("The sum of the array is: "+sum);
+        System.out.println("The Average of the array is: "+average);
+    }
+}
