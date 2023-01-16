e9cdaec8f6befd2c1c1226e9bfe4c074cb6b9b64
sanju50201 <sanjithk50201@gmail.com>
2023-01-16 19:54:53 +0530

    added Generics in Java

---
+++ src/oop/generics/WildCardExample.java
@@ -0,0 +1,64 @@
+package oop.generics;
+
+import java.util.Arrays;
+
+public class WildCardExample<T extends Number> {
+    private Object[] data;
+    private static int DEFAULT_SIZE = 10;
+    private int size = 0;
+
+    public WildCardExample() {
+        this.data = new Object[DEFAULT_SIZE];
+    }
+
+    public void add(T num){
+        if(isFull()){
+            resize();
+        }
+        data[size++] = num;
+    }
+
+    private void resize(){
+        Object[] temp =  new Object[data.length *2];
+
+//        copy the parent element to the temp array
+        for (int i = 0; i < data.length; i++) {
+            temp[i] = data[i];
+        }
+        data = temp;
+    }
+    private boolean isFull() {
+        return size == data.length;
+    }
+
+    public T remove(){
+        T removed = (T)(data[size--]);
+        return removed;
+    }
+
+    public T get(int index){
+        return (T)(data[index]);
+    }
+    public int size(){
+        return size;
+    }
+    public void set(int index, T value){
+        data[index] = value;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomGenArrayList{" +
+                "data=" + Arrays.toString(data) +
+                ", size=" + size +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        WildCardExample<Integer> list = new WildCardExample<>();
+        for (int i = 0; i < 14; i++) {
+            list.add(i * 3);
+        }
+        System.out.println(list);
+    }
+}
