844dc69585435608d5d097474eacd870663700cc
sanju50201 <sanjithk50201@gmail.com>
2023-01-25 17:14:47 +0530

    added a few Cyclic sort Problems Set 2

---
+++ src/sorting/Questions/DuplicatesInArray.java
@@ -0,0 +1,65 @@
+/*
+* Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+
+You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [2,3]
+Example 2:
+
+Input: nums = [1,1,2]
+Output: [1]
+Example 3:
+
+Input: nums = [1]
+Output: []
+
+
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+1 <= nums[i] <= n
+Each element in nums appears once or twice.
+*
+* */
+
+package sorting.Questions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DuplicatesInArray {
+    public static List<Integer> findDuplicates(int[] nums) {
+        int i = 0;
+        while(i < nums.length){
+            int correct = nums[i] - 1;
+            if(nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            }else{
+                i++;
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for(int index = 0; index < nums.length; index++){
+            if(nums[index] != index +1 ){
+                result.add(nums[index]);
+            }
+        }
+        return result;
+    }
+    private static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,7,8,2,3,1};
+        System.out.println(findDuplicates(arr));
+    }
+
+}
