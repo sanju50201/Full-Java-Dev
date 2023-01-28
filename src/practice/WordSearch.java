4599e054eb04917c86f27a60ea7855085338468a
sanju50201 <sanjithk50201@gmail.com>
2023-01-28 12:35:32 +0530

    added a word search question

---
+++ src/practice/WordSearch.java
@@ -0,0 +1,80 @@
+/*
+* Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+
+
+Example 1:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+Example 2:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+Example 3:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+
+
+Constraints:
+
+m == board.length
+n = board[i].length
+1 <= m, n <= 6
+1 <= word.length <= 15
+board and word consists of only lowercase and uppercase English letters.
+
+
+Follow up: Could you use search pruning to make your solution faster with a larger board?
+* */
+
+package practice;
+
+public class WordSearch {
+    public static boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || word == null || word.length() == 0) {
+            return false;
+        }
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, visited, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean dfs(char[][] board, boolean[][] visited, int i, int j, String word, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+        visited[i][j] = true;
+        boolean res = dfs(board, visited, i + 1, j, word, index + 1)
+                || dfs(board, visited, i - 1, j, word, index + 1)
+                || dfs(board, visited, i, j + 1, word, index + 1)
+                || dfs(board, visited, i, j - 1, word, index + 1);
+        visited[i][j] = false;
+        return res;
+    }
+    public static void main(String[] args) {
+        char[][] board = {
+                {'A','B','C','E'},
+                {'S','F','C','S'},
+                {'A','D','E','E'}
+        };
+        String word = "ABCCED";
+        System.out.println(exist(board,word));
+    }
+}
